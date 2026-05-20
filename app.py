b47576448094733c5110046171f91465a07c98e1
shivansha346-ux <shivansha346@gmail.com>
2026-05-20 20:19:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,13 +54,17 @@ input_data = input_data[model_features]
 # लाइव प्रेडिक्शन बटन
 if st.button("Calculate Churn Risk Score"):
     prob = model.predict_proba(input_data)[:, 1]
+    
     # Extracting the single probability value safely from the model array
-if isinstance(prob, (list, np.ndarray)):
-    risk_score = float(prob[0]) * 100
-else:
-    risk_score = float(prob) * 100
-risk_score = int(risk_score)
+    if isinstance(prob, (list, np.ndarray)):
+        risk_score = float(prob[0]) * 100
+    else:
+        risk_score = float(prob) * 100
+        
+    risk_score = int(risk_score)
+    
     st.metric(label="📊 Real-Time ML Risk Score", value=f"{risk_score} / 100")
+    
     if risk_score >= 61:
         st.error("🚨 HIGH RISK ZONE: Customer is highly likely to churn. Immediate retention action needed.")
     elif risk_score >= 31:
